6f740a3e87dece83bc7c11fa5577137c06283e5a
Carson Trent Long <CarsonLong@IOS-Syncy.local>
2015-08-03 14:27:19 +0800

    ft(to-beta): major style changes

--- Public/js/log-in-app.js
+++ Public/js/log-in-app.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+  var User = function(name,email,username,password) {
+    this.name = name;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+  } 
+
+  $(document).on('click','#log-in',function() {
+    var name = $('input')[0].value;
+    var email = $('input')[1].value;
+    var curUser = new User(name,email,null,null);
+    curUser.
+  })
 
   $(document).on('click','#sign-up',function() {
     $.ajax({
